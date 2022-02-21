000a9933cc581bacb6f5ef8e487f85b64d2ce171
Yottaphy <jorge13romero@gmail.com>
2022-02-21 14:17:40 +0200

    increased the search window from 100 to 150 keV

--- build/lib/glados/searcher.py
+++ build/lib/glados/searcher.py
@@ -3,7 +3,7 @@ from .daughter import alphaDaughter
 def lookup(energy, dicNuc):
     output = []
     for _,isotope in dicNuc.items():
-        if isotope.alpha > energy-100 and isotope.alpha < energy+100:
+        if isotope.alpha > energy-150 and isotope.alpha < energy+150:
             output.append(isotope)
     return output
 
@@ -12,6 +12,6 @@ def possibleSums(energy, dicNuc):
     for _,isotope in dicNuc.items():
         if alphaDaughter(isotope, dicNuc) != False:
             candidate = isotope.alpha + alphaDaughter(isotope, dicNuc).alpha 
-            if candidate > energy-100 and candidate < energy+100:
+            if candidate > energy-300 and candidate < energy+300:
                 output.append(isotope)
     return output
