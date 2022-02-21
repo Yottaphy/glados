139a792e82d0978cea1cff02188aea32546f9a34
Yottaphy <jorge13romero@gmail.com>
2022-02-21 16:05:54 +0200

    updated README after uploading to pip

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
-setup(name='GLaDOS',
+setup(name='GLaDOS alpha',
       version='1.0',
       description='General Lookup of α-Decay for Optimised Search (GLαDOS)',
       url='https://github.com/Yottaphy/glados',
       author='Jorge Romero',
       author_email='joromero@jyu.fi',
       license='GPL-3.0',
-      packages=['glados'],
+      packages=['glados_alpha'],
       install_requires=[
           'argparse',
       ],
       zip_safe=False,
       entry_points={
         'console_scripts': [
-            'glados = glados:main'
+            'glados_alpha = glados_alpha:main'
         ]},
     )
