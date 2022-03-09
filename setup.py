90b00b496d12b1a870fc3790ec001f5cc42e91be
Yottaphy <jorge13romero@gmail.com>
2022-03-09 19:01:38 +0200

    Version 2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='GLaDOS alpha',
-      version='1.3',
+      version='2.0',
       description='General Lookup of α-Decay for Optimised Search (GLαDOS)',
       url='https://github.com/Yottaphy/glados',
       author='Jorge Romero',
